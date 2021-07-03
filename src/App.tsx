5e01f47c675d20fadeae40d3e520e80263f5f2d0
olaftarlowski <olaf.tarlowski@gmail.com>
2021-07-03 21:52:37 +0200

    apollo client set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+  from,
+} from "@apollo/client"; 
+import { onError } from "@apollo/client/link/error";
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.map(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    );
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
+const link = from([
+  errorLink,
+  new HttpLink({ uri: "https://countries.trevorblades.com/" }),
+]);
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link,
+});
 
 function App() {
   return (
-    <div className="App">
+    <ApolloProvider client={client}>
+      <div className="App">
       <h1>Hello</h1>
     </div>
+    </ApolloProvider>
   );
 }
 
