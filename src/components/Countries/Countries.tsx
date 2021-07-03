7f74bd0e902ebd01bda8fd9150ff845d62e3141d
olaftarlowski <olaf.tarlowski@gmail.com>
2021-07-03 23:21:10 +0200

    countries component styling improved, back button added

--- src/components/Countries/Countries.tsx
+++ src/components/Countries/Countries.tsx
@@ -1,7 +1,9 @@
 import { useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
-import { LOAD_CONTINENTS_COUNTRIES } from '../../GraphQL/Queries'
+import { LOAD_CONTINENTS_COUNTRIES } from '../../GraphQL/Queries';
+import { Link } from "react-router-dom";
 
+import styles from './Countries.module.css'
 
 
 interface CountryLanguages {
@@ -27,43 +29,53 @@ interface ContData {
 
 interface ParamTypes {
     continentCode: string
-  }
+}
 
 const Countries = () => {
 
 
-    const {error, loading, data } = useQuery<ContData>(LOAD_CONTINENTS_COUNTRIES);
+    const { error, loading, data } = useQuery<ContData>(LOAD_CONTINENTS_COUNTRIES);
 
     const params = useParams<ParamTypes>();
 
-    const matchContinent = data?.continents.find( cont => {
-       return cont.code === params.continentCode;
+    const matchContinent = data?.continents.find(cont => {
+        return cont.code === params.continentCode;
     })
 
     let content = <div>
         loading.....
     </div>;
 
-    if (data) {
-        content = <div>
-            {matchContinent?.countries.map( e => {
-            return <div>{e.name}</div>
-        })}
+    if (matchContinent !== undefined) {
+        content = <div className={styles.wrapper}>
+            {matchContinent?.countries.map(e => {
+                return <div>{e.name}</div>
+            })}
         </div>
+    } else if (!loading && params.continentCode) {
+        content = <div>Page not found. Consider another path.</div>;
     }
 
     const check = () => {
         console.log(data?.continents);
         console.log(matchContinent);
     }
-    
+
 
     return (
-        <div>
-            <button onClick={check}>check</button>
-            {params.continentCode}
-            {content}
-        </div>
+        <>
+            <div className={styles.btnContainer}>
+                <Link to="./">
+                    <button className={styles.backBtn}>back</button>
+                </Link>
+                <h2>Current continent code: {params.continentCode}</h2>
+            </div>
+            <div>
+                <button onClick={check}>check</button>
+                {error && <h3> {error.message} </h3>}
+                {content}
+            </div>
+        </>
     )
 }
 
